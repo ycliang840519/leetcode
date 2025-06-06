db7cf818a430abdb8d17d5046f38e2af28b9d2b7
Leo Yu-Chi Liang <ycliang@andestech.com>
2025-06-06 11:57:48 +0800

    Add 733 Flood Fill
    Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>

---
+++ grind-169/733/solution.cpp
@@ -0,0 +1,61 @@
+// DFS, resursive
+
+class Solution {
+public:
+    void dfsFill(vector<vector<int>>& image, int sr, int sc, int color, int origColor) {
+        int m = image.size(), n = image[0].size();
+        if (sr >= m || sc >= n || sr < 0 || sc < 0) // not inside the image
+            return;
+        
+        if (image[sr][sc] != origColor)             // not the same pixel
+            return;
+
+        if (origColor == color)                     // already the target color
+            return;
+        
+        image[sr][sc] = color;
+
+        dfsFill(image, sr + 1, sc,     color, origColor);
+        dfsFill(image, sr,     sc + 1, color, origColor);
+        dfsFill(image, sr - 1, sc,     color, origColor);
+        dfsFill(image, sr,     sc - 1, color, origColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        dfsFill(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+};
+
+// BFS, iterative
+
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size(), n = image[0].size(), origColor = image[sr][sc];
+        
+        queue<pair<int, int>> bfsQueue;
+        bfsQueue.push(make_pair(sr,sc));
+
+        while (!bfsQueue.empty()) {
+            pair<int, int> curr = bfsQueue.front();
+            bfsQueue.pop();
+
+            // outside of image
+            if (curr.first >= m || curr.second >= n || curr.first < 0 || curr.second < 0) 
+                continue;
+            if (image[curr.first][curr.second] != origColor)    // pixel not same
+                continue;
+            if (image[curr.first][curr.second] == color)    // already filled
+                continue;
+
+            image[curr.first][curr.second] = color;
+
+            bfsQueue.push(make_pair(curr.first + 1, curr.second));
+            bfsQueue.push(make_pair(curr.first    , curr.second + 1));
+            bfsQueue.push(make_pair(curr.first - 1, curr.second));
+            bfsQueue.push(make_pair(curr.first    , curr.second - 1));
+        }
+
+        return image;
+    }
+};
